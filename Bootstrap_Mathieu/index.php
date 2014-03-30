4eb605c4c32c4ca0a709b490f3fa7719deaede5e
Mathieu SPEGAGNE <mathieu.sge@gmail.com>
2014-03-30 22:59:12 +0200

    essai bootstrap

---
+++ Bootstrap_Mathieu/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	
+    <!-- Custom styles for this template -->
+    <link href="template.css" rel="stylesheet">
+	
+	<title>Starter Template for Bootstrap</title>
+	
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  
+  
+ <body>
+ 
+ 
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">MySitek</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	
+	  <div class="container principal">
+      <header class="row">
+        <div class="col-sm-12">
+         <p>
+            Je suis passionné par les <strong>tigres</strong> depuis très longtemps. Ce site a été construit en <em>hommage à ces merveilleux félins...</em><br>
+            Je fais partie de la <abbr title="Société des Amoureux des Tigres">SAT</abbr> qui a pour but de faire connaître ces splendides animaux. 
+          </p>
+          <p>Voici ce qu'en dit le wikipedia :</p>
+          <blockquote>
+            Le Tigre (Panthera tigris) est un mammifère carnivore de la famille des félidés (Felidae) du genre Panthera. 
+Aisément reconnaissable à sa fourrure rousse rayée de noir, il est le plus grand félin sauvage et l'un des plus grands carnivores du monde. 
+L'espèce est divisée en neuf sous-espèces possédant des différences mineures en termes de taille ou de comportement. Superprédateur, 
+il chasse principalement les cerfs et les sangliers, bien qu'il puisse s'attaquer à des proies de taille plus importante comme les buffles. 
+Jusqu'au XIXe siècle, le Tigre était réputé mangeur d'homme. La structure sociale des tigres en fait un animal solitaire ; le mâle possède 
+un territoire qui englobe les domaines de plusieurs femelles et ne participe pas à l'éducation des petits.<br>
+            <small class="pull-right">Wikipedia</small><br>
+          </blockquote>
+        </div>
+      </header>
+      <div class="row">
+        <nav class="col-sm-2">
+          Menu
+        </nav>
+        <section class="col-sm-10">
+          <div class="row">
+            <article class="col-md-10">
+              <section class="row">
+        <div class="col-sm-2"><img src="images/t1.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t2.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t3.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t4.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t5.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t6.jpg" alt="Tigre"></div>
+		</section>
+		<section class="row">
+        <div class="col-sm-2"><img src="images/t7.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t8.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t9.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t10.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t11.jpg" alt="Tigre"></div>
+        <div class="col-sm-2"><img src="images/t12.jpg" alt="Tigre"></div>
+      </section>
+            </article>
+            <aside class="col-md-2">
+              <blockquote>Le Tigre (Panthera tigris) est un mammifère carnivore...<br>
+<small class="pull-right">Wikipedia</small><br>
+</blockquote>
+            </aside>
+          </div>
+        </section>
+      </div>
+      <footer class="row">
+        <div class="col-sm-12">
+         <address>
+   <p>Vous pouvez me contacter à cette adresse :</p>
+   <p><strong>Tigrou Alfred</strong><br>
+     Allée des fauves<br>
+     28645 Félins-sur-Loire<br>
+</address>
+        </div>
+      </footer>
+    </div><!-- /.container -->
+	
+	
+    <script src="bootstrap/js/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>

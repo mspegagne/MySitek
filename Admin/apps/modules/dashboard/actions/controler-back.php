a4a1fd1cc3921b5e2b1384e9d7c278592fd18e18
Mathieu SPEGAGNE <mathieu.sge@gmail.com>
2014-06-30 22:49:04 +0200

    Amelioration archi + debut UI

--- Admin/apps/modules/dashboard/actions/controler-back.php
+++ Admin/apps/modules/dashboard/actions/controler-back.php
@@ -2,16 +2,15 @@
 $dashboard = $app['controllers_factory'];
 
 
-/* Activation de twig avec les templates du module */
-
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.class_path'    => __DIR__ . '/../../../../vendor/Twig/lib',
-        'twig.path' => array(__DIR__.'/../../../templates/'.$app['template'].'/',
-                             __DIR__.'/../templates/',)
-)); 
-
-
 $dashboard->get('/', function() use ($app) {
+    
+    /* Activation de twig avec les templates du module */
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+            'twig.class_path'    => __DIR__ . '/../../../../vendor/Twig/lib',
+            'twig.path' => array(__DIR__.'/../../../templates/'.$app['template'].'/',
+                                 __DIR__.'/../templates/',)
+    )); 
     
     //Recuperation de l'user
     $app['user'] =  $app['security']->getToken()->getUser() ;    

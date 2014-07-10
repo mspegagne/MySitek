399782fa3ddaffae5c117f652a179332a8315e00
Mathieu SPEGAGNE <mathieu.sge@gmail.com>
2014-07-10 22:38:42 +0200

    scroll + détail

--- Site-vitrine/js/mysitek.js
+++ Site-vitrine/js/mysitek.js
@@ -7,19 +7,13 @@ $(window).scroll(function() {
     }
 });
 
-//   Script pour le scroll en douceur entre les ancres...
+//jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 500);
-                return false;
-            }
-        }
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
     });
 });

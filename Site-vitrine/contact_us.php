5881e85a6f426f8d3751a3ff6372386777ffd102
Bruno MATRY <brunomatry@gmail.com>
2014-07-20 15:32:04 +0200

    [Site-Vitrine] Creation du formulaire de contact

---
+++ Site-vitrine/contact_us.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <?php include_once "./share/head.php"; ?>
+
+    <body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+        <?php
+        $preciseLink = true;
+        include_once "./share/nav.php";
+        ?>
+
+        <section id="contact_us" class="content-section">
+            <div class="container">
+                <div class="col-lg-offset-2 col-lg-8">
+                    <div class="clearfix"></div>
+                    <h2>Nous contacter</h2>                  
+
+                    <form role="form" action="contact_us_post.php" method="post" >
+                        <div class="well well-sm"><strong><i class="glyphicon glyphicon-ok form-control-feedback"></i> Champs requis</strong></div>
+                        <div class="form-group">
+                            <label for="InputName">Votre nom</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="InputName" id="InputName" placeholder="Entrez votre nom" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="InputEmail">Votre email</label>
+                            <div class="input-group">
+                                <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Entrez votre email" required  >
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="InputMessage">Votre message</label>
+                            <div class="input-group">
+                                <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required></textarea>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="InputReal">Combien font 4+3 ?</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="InputReal" id="InputReal" required>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+                            </div>
+                        </div>
+                        <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info center-block">
+                    </form>
+                </div>
+            </div>
+        </section>
+
+        <section id="banner" class="content-section">
+            <div class="banner">
+                <div class="clearfix"></div>
+                <div class="container">
+                    <div class="row">
+                        <!--<h3>Lancement prévu fin 2014. Vous souhaitez etre tenu au courant de l'avancement de notre projet ? Laissez-nous votre email !</h3>
+                        <form role="form" action="" method="post" >
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Entrez votre email">
+                            </div>
+                            <input type="submit" name="submit" id="submit" value="Valider" class="btn btn-warning center-block">
+                        </form>-->
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php
+        include_once "./share/footer.php";
+        include_once "./share/scripts.php";
+        ?>
+
+    </body>
+</html>
+
---
+++ Site-vitrine/index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <?php include_once "./share/head.php"; ?>
+
+    <body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+        <?php
+        $preciseLink = false;
+        include_once "./share/nav.php";
+        ?>
+
+        <section id="intro" class="intro">            
+            <a id="welcome"></a>
+            <div class="intro-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 col-sm-12 intro-message">
+                            <h1>Votre site web personnalisé en toute simplicité</h1>
+                            <h3>Lancement prévu fin 2014 !</h3>
+                            <span class="page-scroll">
+                                <a href="#presentation" class="btn btn-circle">
+                                    <i class="fa fa-angle-double-down animated"></i>
+                                </a>
+                            </span>
+                            <br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="artisan" class="content-section left">
+            <a id="presentation" style="position:relative; top: -90px;"></a>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-sm-6">
+                        <div class="clearfix"></div>
+                        <h2>Vous êtes ...</h2>
+                        <div class="row">
+                            <ul>        
+                                <li class="col-md-4 col-xs-12">
+                                    <span class="round-tabs">
+                                        <i class="fa fa-user"></i>
+                                    </span>
+                                    Entrepreneur,<br>
+                                    Particulier
+                                </li>
+                                <li class="col-md-4 col-xs-12">
+                                    <span class="round-tabs">
+                                        <i class="fa fa-cube"></i>
+                                    </span>
+                                    Artisan,<br>
+                                    Artiste
+                                </li>
+                                <li class="col-md-4 col-xs-12">
+                                    <span class="round-tabs">
+                                        <i class="fa fa-suitcase"></i>
+                                    </span>
+                                    Profession<br>
+                                    libérale     
+                                </li>
+                            </ul> 
+                        </div>
+                        <p class="lead">
+                            Vous avez une activité que vous souhaitez mettre en avant sur internet, vous voulez <strong>figurer en premier dans les résultats sur Google, Bing, ...</strong> Vous êtes perdus parmis des offres gratuites ou payantes qui ne répondent pas parfaitement à vos besoins ? <strong>Ne cherchez plus, vous êtes au bon endroit !</strong>
+                        </p> 
+                    </div>
+                    <div class="col-lg-5 col-lg-offset-1 col-sm-6 screen hidden-xs">
+                        <img class="img-responsive img1" src="img/faces.jpg" alt="faces">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="site" class="content-section right">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6  col-sm-push-6  col-sm-6">
+                        <div class="clearfix"></div>
+                        <h2>Vous recherchez à avoir un site...</h2>
+                        <div class="row">
+                            <ul>        
+                                <li class="col-md-4 col-xs-12">
+                                    <span class="round-tabs">
+                                        <i class="fa fa-gear"></i>
+                                    </span>
+                                    Entièrement<br/>
+                                    personnalisé
+                                </li>
+                                <li class="col-md-4 col-xs-12">
+                                    <span class="round-tabs">
+                                        <i class="fa fa-rocket"></i>
+                                    </span>
+                                    Moderne et<br/>
+                                    responsive
+                                </li>
+                                <li class="col-md-4 col-xs-12">
+                                    <span class="round-tabs">
+                                        <i class="fa fa-dollar"></i>
+                                    </span>
+                                    Au meilleur<br/>
+                                    prix
+                                </li>
+                            </ul>
+                        </div>
+                        <p class="lead">
+                            Vous ne voulez pas avoir le même site que Monsieur Toutlemonde. Vous souhaitez un site <strong>sans publicités</strong> qui soit <strong>compatible mobile et tablette</strong>. Vous souhaitez avoir un minimum d'indépendance lorsque vous souhaitez simplement modifier un texte ou une image. <strong>Gagnez du temps et de l'argent en adoptant MySitek !</strong>
+                        </p> 
+                    </div>
+                    <div class="col-lg-5 col-sm-pull-6 col-sm-6 screen hidden-xs">
+                        <img class="img-responsive img2" src="img/tablette.jpg" alt="tablette">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section id="solution" class="content-section left">            
+            <a id="mysitek" style="position:relative; top: -90px;"></a>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-xs-12">
+                        <div class="clearfix"></div>
+                        <h2>Ce que MySitek vous propose</h2>                  
+                        <p class="lead">
+                            MySitek est un outil vous permettant de <strong>créer un site internet adapté à vos besoins en 3 clics</strong>.<br/>
+                            La particularité de MySitek est que vous conservez la main sur votre site internet. En effet, après avoir choisi votre base, vous pouvez choisir d'héberger vous-même votre site. Vous en disposez alors de façon illimitée et gratuite. Vous pourrez continuer de faire évoluer votre offre en sélectionnant de nouveaux modules ou en mettant à jour votre base.
+                        </p>
+                    </div>
+                    <div class="col-lg-5 col-lg-offset-1 col-md-6 col-md-offset-0 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2">
+                        <img class="img-responsive no-border" src="img/mysitek-icone.png" alt="mysitek">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="unique" class="content-section right">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6  col-sm-push-6  col-sm-6">
+                        <div class="clearfix"></div>
+                        <h2>Pour MySitek, chaque site est unique    </h2>
+                        <p class="lead">
+                            <strong>Plusieurs bases</strong> vous serons proposées afin de répondre au mieux à vos besoins. Les <strong>modules</strong> (slider pour présenter vos créations, bibliothèque d'images, ...) qui vous seront proposés pourront être <strong>ajoutés ou retirés à votre guise</strong>. Nous vous proposons un choix varié de thèmes modernes et responsive (adaptés aux ordinateurs, tablettes et mobiles) que vous pourrez <strong>personnaliser en quelques clics</strong>.
+                        </p>
+                    </div>
+                    <div class="col-lg-5 col-sm-pull-6 col-sm-6 screen center">
+                        <i class="fa fa-star"></i>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="pointe" class="content-section left">
+            <a id="presentation" style="position:relative; top: -90px;"></a>
+            <div class="container"> 
+                <div class="row">
+                    <div class="col-lg-6 col-sm-6">
+                        <div class="clearfix"></div>
+                        <h2>Restez à la pointe</h2>
+                        <p class="lead">
+                            Nous vous proposerons des <strong>mises à jour gratuites</strong> pour vos bases, vos modules et vos thèmes. De <strong>nouveaux modules et de nouveaux thèmes</strong> vous seront aussi proposé <strong>régulièrement</strong>.
+                        </p>                          
+                    </div>
+                    <div class="col-lg-5 col-lg-offset-1 col-sm-6 screen center">
+                        <i class="fa fa-rocket"></i>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="nous" class="content-section right">            
+            <a id="who-are-we" style="position:relative; top: -90px;"></a>
+            <div class="container">
+                <div class="row">
+                    <div class="clearfix"></div>
+                    <h2>Qui sommes-nous ?</h2>
+                </div>
+                <div class="row">
+                    <div class="col-lg-2 col-md-4 col-xs-12">
+                        <div class="row center">
+                            <span class="round-tabs profile-picture">
+                                <img class="no-border" src="img/mathieu.png" alt="mathieu">
+                            </span>                            
+                        </div>
+                        <div class="row center">
+                            <a href="https://www.linkedin.com/in/mathieuspegagne/" rel="nofollow" target="_blank">
+                                <span class="round-tabs"><i class="fa fa-linkedin"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-8 col-xs-12">
+                        <h3>Mathieu Spegagne</h3>
+                        <p class="lead">
+                            Elève ingénieur en informatique, Mathieu développe des sites depuis plus de 6 ans. 
+                            Actuellement président de la Junior-Entreprise de son école, il a vu évoluer la demande 
+                            des artisans et des associations en matière de site simples et personnalisables. C'est pourquoi il a eu
+                            l'idée de s'associer avec Bruno afin de créer la solution MySitek.
+                        </p>
+                    </div>
+                    <div class="col-lg-2 col-md-4 col-xs-12">
+                        <div class="row center">
+                            <span class="round-tabs profile-picture">
+                                <img class="no-border" src="img/bruno.png" alt="bruno">
+                            </span>
+                        </div>
+                        <div class="row center">
+                            <a href="https://fr.linkedin.com/pub/bruno-matry/56/768/43b" rel="nofollow" target="_blank">
+                                <span class="round-tabs"><i class="fa fa-linkedin"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-8 col-xs-12">
+                        <h3>Bruno Matry</h3>
+                        <p class="lead">
+                            Fils de musiciens et petit-fils d'artisan peintre, Bruno est étudiant en informatique à l'INSA de Rennes. Il est fortement impliqué dans le monde associatif aussi bien dans le domaine de l'évènementiel (Rock'n Solex, InsaLan) que dans l'entreprenariat (Junior-Entreprise).</br>Il s'intéresse plus particulièrement aux technologies du web et est donc fortement impliqué dans ce projet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="banner" class="content-section">
+            <div class="banner">
+                <div class="clearfix"></div>
+                <div class="container">
+                    <div class="row">
+                        <!--<h3>Lancement prévu fin 2014. Vous souhaitez etre tenu au courant de l'avancement de notre projet ? Laissez-nous votre email !</h3>
+                        <form role="form" action="" method="post" >
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Entrez votre email">
+                            </div>
+                            <input type="submit" name="submit" id="submit" value="Valider" class="btn btn-warning center-block">
+                        </form>-->
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php
+        include_once "./share/footer.php";
+        include_once "./share/scripts.php";
+        ?>
+
+    </body>
+</html>

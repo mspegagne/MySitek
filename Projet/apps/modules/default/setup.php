d6aa7f324b0cb621a4088c5c4b9575f9f7e6a776
mspegagne <mathieu.sge@gmail.com>
2014-08-01 18:10:45 +0200

    interface maj

--- Projet/apps/modules/default/setup.php
+++ Projet/apps/modules/default/setup.php
@@ -3,9 +3,16 @@
 $sql = "SELECT * FROM modules WHERE lien = '2048'";
 $retour = $app['db']->fetchAssoc($sql);
 
-if($retour == '')
-{
-$sql = "INSERT INTO modules (rang, name, lien, icon, front, selected, version) VALUES (?,?,?,?,?,?,?)";
-$app['db']->executeUpdate($sql, array(0,'2048', '2048', 'fa fa-gamepad', 2, 1, 0.1));
+if ($retour == '') {
+    
+    $sql = "INSERT INTO modules (rang, name, lien, icon, front, selected, version) VALUES (?,?,?,?,?,?,?)";
+    $app['db']->executeUpdate($sql, array(0, '2048', '2048', 'fa fa-gamepad', 2, 1, 0.1));
+    
+} 
 
+elseif ($retour['selected'] == 0) {
+   
+        $sql = "UPDATE modules SET selected = 1 WHERE lien = '2048'";
+        $app['db']->executeUpdate($sql, array());
+   
 }

6f1fe4e42c3b6470609c1bf1096dd01c7d15e850
mspegagne <mathieu.sge@gmail.com>
2014-09-02 19:57:18 +0200

    selected

--- Projet/apps/modules/accueil/actions/controler-front.php
+++ Projet/apps/modules/accueil/actions/controler-front.php
@@ -15,7 +15,7 @@ $accueil->get('/', function() use ($app) {
             __DIR__ . '/../templates/',)
     ));
 
-    $app['selected'] = 'default';
+    $app['selected'] = 'accueil';
 
     return $app['twig']->render('front.twig', array(
                'hello' => 'Hello world Front ! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.'
